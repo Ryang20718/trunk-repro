05803d0819740933ec135526591c08920e034919
ryan <ryang@waabi.ai>
2023-05-17 22:14:24 -0700

    test

--- a.py
+++
@@ -1,8 +0,0 @@
-import os
-print(os.getenv("GG"))
-print(os.getenv("GG"))
-print(os.getenv("GG"))
-print(os.getenv("GG"))
-k: str = 3
-print(os.getenv("GG"))
-print("hello")
---
+++ test/a.py
@@ -0,0 +1,29 @@
+"""
+
+input = int(input("Enter a number: ")).
+
+asdasd
+
+"""
+import os
+
+print(os.getenv("GG"))
+print(os.getenv("GG"))
+print(os.getenv("GG"))
+print(os.getenv("GG"))
+print(os.getenv("GG"))
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+k: str = 3
+print(os.getenv("GG"))
+
+def kk() ->int:
+    """
+    _summary_.
+
+    Returns:
+        int: _description_
+    """
+    return "gg"
