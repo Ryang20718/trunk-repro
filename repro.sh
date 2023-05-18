dcf62981bfc6477bd655775b66219d325c7dcdcd
ryan <ryang@waabi.ai>
2023-05-17 23:13:29 -0700

    u

--- repro.sh
+++ repro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 m venv .venv && source .venv/bin/activate && pip3 install -r requirements.txt
+python3 -m venv .venv && source .venv/bin/activate && pip3 install -r requirements.txt
 git fetch origin main && git reset --hard origin/main && git checkout -b testing_error
 echo 'g = 3' >> test/a.py
 cp test/b.py test/e.py && cp test/b.py test/f.py
